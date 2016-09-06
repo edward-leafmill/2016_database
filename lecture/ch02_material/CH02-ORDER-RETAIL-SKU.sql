6726a3ff1ab04ab2ba2864b435d41540899e547d
edward <agile.excy@gmail.com>
2016-09-06 23:32:07 +0900

    Add some sql statments to create database named 'cape_codd'

--- lecture/ch02_material/CH02-ORDER-RETAIL-SKU.sql
+++ lecture/ch02_material/CH02-ORDER-RETAIL-SKU.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `cape_codd` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `cape_codd`;
 -- MySQL dump 10.13  Distrib 5.6.22, for osx10.8 (x86_64)
 --
 -- Host: 127.0.0.1    Database: cape_codd
@@ -108,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-06 11:46:40
+-- Dump completed on 2016-09-06 23:30:07
